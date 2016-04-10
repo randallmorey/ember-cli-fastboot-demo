49b878e0ba9b9c6f04689908388954a4b861480b
Randall Morey <randall@randallmorey.com>
2016-04-10 09:49:00 -0700

    added .env-based S3 deploy config

--- config/deploy.js
+++ config/deploy.js
@@ -2,7 +2,13 @@
 
 module.exports = function(deployTarget) {
   var ENV = {
-    build: {}
+    build: {},
+    s3: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      bucket: process.env.AWS_S3_BUCKET,
+      region: process.env.AWS_S3_REGION
+    }
     // include other plugin configuration that applies to all deploy targets here
   };
 
